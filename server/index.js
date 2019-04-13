8a8c8edee6a5ff8e9486cf7ea8f70df6f9698121
Václav Hoštička <venca163@gmail.com>
2019-04-13 17:21:56 +0200

    init express server (serves client/index.html)

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const chalk = require('chalk')
+const express = require('express')
+const path = require('path')
+
+const port = 4000
+
+
+const init = () => {
+	console.log('initing server...')
+	const app = express()
+
+	app.get('/client', (req, res) => {
+		res.sendFile(path.join(__dirname + '/../client/index.html'))
+	})
+
+	app.listen(port, () => {
+		console.log(chalk.cyan(`Server listening on port ${port}`))
+	})
+}
+
+module.exports = {
+	init,
+}
